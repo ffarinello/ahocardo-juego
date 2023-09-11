9cd5e1066cc8c898f3ff3c0f1672c453f13d993f
ffarinello <farinello.gg@gmail.com>
2023-09-11 03:37:53 -0300

    Arranco con js

---
+++ script.js
@@ -0,0 +1,25 @@
+const palabrasRandom = [
+    "Mariposa",
+    "Teléfono",
+    "Montaña",
+    "Sombra",
+    "Radiante",
+    "Candelabro",
+    "Plátano",
+    "Espiral",
+    "Piano",
+    "Murciélago",
+];
+
+//CREO UN DIV DE EJEMPLO
+const div = document.createElement("div");
+div.textContent = "Hecho con JS";
+const seccion = document.querySelector(".contenedor");
+
+seccion.appendChild(div);
+
+console.log(div.textContent);
+
+// Crear un div por cada letra que tenga la palabra.
+// Los divs deben tener un valor por defecto (espacio vacío).
+// Los divs deben tener un 'underline' donde iría la letra.
